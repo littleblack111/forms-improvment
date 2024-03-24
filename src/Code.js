f58ce38940c4cb0a67011012c310bfe35c14b502
mrdog233o5 <mr.dog233o5@gmail.com>
2024-03-24 10:19:07 +0800

    automatic create feedback from the forms

--- src/Code.js
+++ src/Code.js
@@ -2,29 +2,32 @@ function main() {
     const spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
     const reply = spreadsheet.getSheetByName('Reply'); // Get the sheet named "Reply"
     const feedback = spreadsheet.getSheetByName('Feedback'); // Get the sheet named "Reply"
-    const reply_data = reply.getDataRange().getValues(); // Get all data in the sheet
+    var reply_data = reply.getDataRange().getValues(); // Get all data in the sheet
     const feedback_data = feedback.getDataRange().getValues(); // Get all data in the sheet
     const userEmail = Session.getActiveUser().getEmail(); // Get current user's email
+    var result = [];
+
+    reply_data.shift();
     
     // Assuming the email is in the first column (index 0)
     for (let i = 0; i < reply_data.length; i++) {
         if (reply_data[i][1] === userEmail) {
-            reply_data[i].shift()
-            reply_data[i].shift()
-            data = reply_data[i]
-            console.log(feedback_data)
+            reply_data[i].shift();
+            reply_data[i].shift();
+            data = reply_data[i];
+            console.log(data);
+            console.log(feedback_data);
             for (let j = 0; j < data.length; j++) {
-                for (let k = 0; k < feedback_data.length; k++) {
-                    console.log(j, k)
+                for (let k = 0; k < feedback_data[j].length; k+=2) {
                     if (data[j] == feedback_data[j][k]) {
-                        console.log(j, k)
+                        console.log(j, k);
+                        console.log(feedback_data[j][k + 1]);
+                        result.push(feedback_data[j][k + 1]);
+                        break; // Assuming there's only one match, we can break the loop once found
                     }
                 }
-                // if reply_data[i][j] === filter_data[j] {
-                    
-                // }
             }
-            break; // Assuming there's only one match, we can break the loop once found
         }
     }
+    console.log(result);
 }
